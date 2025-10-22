6df16983508ea0993cb369b942301ce426b6f5ab
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2025-10-22 14:39:36 +0000

    Search console manenos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,34 @@
-"use client";
-
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "@/components/theme-provider";
-import { AuthProvider } from "@/context/auth-context";
+import "./globals.css";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "PexPay | Seamless M-Pesa Payments API",
+  description:
+    "Powerful, secure, and developer-friendly API for M-Pesa STK Push integration. Seamlessly accept M-Pesa payments in your application.",
+  verification: {
+    google: "EuJXRpbWcnXPIDkyL3ox6DnpDF3bPRys-5FGbcjE1ac",
+  },
+  openGraph: {
+    title: "PexPay | Seamless M-Pesa Payments API",
+    description:
+      "Powerful, secure, and developer-friendly API for M-Pesa STK Push integration. Seamlessly accept M-Pesa payments in your application.",
+    url: "https://pay.pexmon.one",
+    siteName: "PexPay",
+    images: [
+      {
+        url: "https://pay.pexmon.one/og-image.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -17,17 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <AuthProvider>
-            {children}
-            <Toaster />
-          </AuthProvider>
-        </ThemeProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
